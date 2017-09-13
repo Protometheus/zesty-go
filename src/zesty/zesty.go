24b54fc9176dcf8a73dd558382931e2980e28f15
Mohammad Oweis <hamada.oweis@gmail.com>
2017-09-12 20:34:55 -0400

    simple webserver test

--- src/zesty/zesty.go
+++ src/zesty/zesty.go
@@ -1,28 +1,13 @@
-// package main
-
-// import (
-//     "fmt"
-//     "net/http"
-// )
-
-// func handler(w http.ResponseWriter, r *http.Request) {
-//     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-// }
-
-// func main() {
-//     http.HandleFunc("/", handler)
-//     http.ListenAndServe(":5000", nil)
-// }
 package main
 
 import (
-
-	"log"
-	// "net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
+    "fmt"
+    "net/http"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -31,17 +16,6 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.New()
-	//router.Use(gin.Logger())
-	//router.LoadHTMLGlob("templates/*.tmpl.html")
-	//router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		// c.HTML(http.StatusOK, "index.tmpl.html", nil)
-        c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	router.Run(":" + port)
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(port, nil)
 }
