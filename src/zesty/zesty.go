83db794d7aab38478f702903290dd810c4878681
Mohammad Oweis <hamada.oweis@gmail.com>
2017-09-12 21:45:41 -0400

    now serving JSON! (consult your doctor before trying)

--- src/zesty/zesty.go
+++ src/zesty/zesty.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"os"
-	// "log"
     "fmt"
     "net/http"
-	
-	// "github.com/gin-gonic/gin"
+	"json"
 )
+
+type Name struct {
+	first 	string
+	last 	string
+}
+
+type PersonalInfo struct {
+	name 				Name
+	github_repo_link 	string
+	website 			string
+	email				string
+}
 
 func base_handler(w http.ResponseWriter, r *http.Request) {
     http.Redirect(w, r, "/code/challenge", 301)
 }
 
 func endpoint_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	personal_info := PersonalInfo{Name{"Mohammad", "Oweis"}, "https://github.com/Protometheus/zesty-go",  "https://github.com/Protometheus/zesty-go", "oweismo.applicant@gmail.com"}
+	
+	js, err := json.Marshal(personal_info)
+	
+	w.WriteHeader(201)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Write(js)
 }
 
 func main() {
@@ -26,11 +43,10 @@ func main() {
 	
 	port = ":" + port
 	
-	// gin.New()
-	
 	fmt.Println(port)
 
     http.HandleFunc("/", base_handler)
 	http.HandleFunc("/code/challenge", endpoint_handler)
+	
     http.ListenAndServe(port, nil)
 }
