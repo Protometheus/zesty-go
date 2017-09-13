b8171c83ba25abee08f943292a8975247b4dc8af
Mohammad Oweis <hamada.oweis@gmail.com>
2017-09-12 20:07:41 -0400

    Update main.go

--- src/zesty/main.go
+++ src/zesty/main.go
@@ -1,15 +1,47 @@
+// package main
+
+// import (
+//     "fmt"
+//     "net/http"
+// )
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// }
+
+// func main() {
+//     http.HandleFunc("/", handler)
+//     http.ListenAndServe(":5000", nil)
+// }
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":5000", nil)
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	//router.Use(gin.Logger())
+	//router.LoadHTMLGlob("templates/*.tmpl.html")
+	//router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		// c.HTML(http.StatusOK, "index.tmpl.html", nil)
+        c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run(":" + port)
 }
