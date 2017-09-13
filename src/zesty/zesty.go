963f8e7689ba4e79763dc14bf6ae901f19dab4f1
Mohammad Oweis <hamada.oweis@gmail.com>
2017-09-12 22:07:59 -0400

    got json to marshal (had to use tags to get them to be lowercase tho >_>

--- src/zesty/zesty.go
+++ src/zesty/zesty.go
@@ -4,19 +4,19 @@ import (
 	"os"
     "fmt"
     "net/http"
-	// "encoding/json"
+	"encoding/json"
 )
 
 type Name struct {
-	first 	string
-	last 	string
+	First 	string	`json:"first"`
+	Last 	string	`json:"last"`
 }
 
 type PersonalInfo struct {
-	name 				Name
-	github_repo_link 	string
-	website 			string
-	email				string
+	Name 				Name	`json:"name"`
+	Github_repo_link 	string	`json:"github_repo_link"`
+	Website 			string	`json:"website"`
+	Email				string	`json:"email"`
 }
 
 func base_handler(w http.ResponseWriter, r *http.Request) {
@@ -24,20 +24,14 @@ func base_handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func endpoint_handler(w http.ResponseWriter, r *http.Request) {
-	// personal_info := PersonalInfo{Name{"Mohammad", "Oweis"}, "https://github.com/Protometheus/zesty-go",  "https://github.com/Protometheus/zesty-go", "oweismo.applicant@gmail.com"}
-	
-	personal_info := []byte(`{"name": {"first": "Mohammad", "last": "Oweis"}, "website": "https://github.com/Protometheus/zesty-go",  "github_repo_link": "https://github.com/Protometheus/zesty-go", "email":"oweismo.applicant@gmail.com"}`)
-	
-	// js, _ := json.Marshal(personal_info)
-	
-	fmt.Println("TESTING PRINT")
-	// fmt.Println(js)
+	personal_info := PersonalInfo{Name{"Mohammad", "Oweis"}, "https://github.com/Protometheus/zesty-go",  "https://github.com/Protometheus/zesty-go", "oweismo.applicant@gmail.com"}
+
+	js, _ := json.Marshal(personal_info)
 	
 	w.WriteHeader(201)
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	// w.Write(js)
-	w.Write(personal_info)
+	w.Write(js)
 }
 
 func main() {
